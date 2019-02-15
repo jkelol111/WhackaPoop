de21b1598d4806796996958b01e67248430f5a73
Nguyen Dev <vietbetatester@outlook.com>
2019-02-15 17:32:56 +0800

    Update changelog.txt and index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Checking cookies happens here.
 try {
   //whackapoop version here.
-  var WHACKAPOOP_VERSION = '0.1.0.2';
+  var WHACKAPOOP_VERSION = '0.1.1';
 
   //Set toastr options here.
   toastr.options = {
@@ -60,6 +60,15 @@ try {
                     .then(text => alert(text))
     } else {
       //Do nothing.
+    }
+  }
+
+  //Logger function (activates verbose when debug mode is ON)
+  function logger(process, type, string) {
+    if (localStorage.getItem('whackapoop_settings_debugModeEnabled') == 'true') {
+      console.log('['+process=': '+type+'] '+string);
+    } else {
+      console.log(string);
     }
   }
 
